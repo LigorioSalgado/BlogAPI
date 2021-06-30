6bc6c0990a2b02cc8fe2a937a9b38906ed605770
Ligorio Edwin Salgado Flores <edwinsalgado@Mac-mini-de-Ligorio.local>
2021-06-29 21:30:50 -0500

    two migrations
    Signed-off-by: Ligorio Edwin Salgado Flores <edwinsalgado@Mac-mini-de-Ligorio.local>

--- data/migrations/20210630012451_users.js
+++ data/migrations/20210630012451_users.js
@@ -1,8 +1,17 @@
 // este archivo sirve como registro de los cambios de la BD
 exports.up = function(knex) {
   // aqui voy a escribir  los cambios que quiero hacer a mi base de datos 
+  return  knex.schema.createTable('users', function (table) {
+      table.increments() // esto es el id y aparte el primary_key
+      table.string('first_name', 150)
+      table.string('last_name',150)
+      table.string('email', 150)
+      table.date('birth_date') // 1993-03-28
+      table.enu('gender',['male', 'female', 'other'])
+  })
 };
 
 exports.down = function(knex) {
   // aqui voy a a escribir  codigo que revierta los cambios de arriba
+  return knex.schema.dropTableIfExists('users')
 };
---
+++ data/migrations/20210630020845_posts.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+  return knex.schemas.createTable('posts', function(table){
+            table.increments()
+            table.string('title', 200)
+            table.text('body','longtext')
+            table.integer('user_id').notNullable().references('id').inTable('users').onDelete('cascade')
+            table.string('category', 100)
+            table.timestamps() 
+  })
+};
+
+exports.down = function(knex) {
+   return knex.schema.dropTableIfExists('posts')
+};
